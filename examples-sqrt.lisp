cd3e69cd0845c4a31f7d99c6ec7babcde667aefe
Olaf Merkert <olaf@m-merkert.de>
2012-07-03 10:59:22 +0200

    look at the (supposedly) non-periodic example

--- examples-sqrt.lisp
+++ examples-sqrt.lisp
@@ -4,3 +4,13 @@
   (let ((cf (make-srcf (make-polynomial
                         1 -8 -42 424 -119))))
     (srcf-quasi-period cf)))
+
+(defun example-char0 (bound)
+  (let ((cf (make-srcf 
+             (make-polynomial 1 0 0 0 0 1 1))))
+    (srcf-quasi-period cf bound)))
+
+(defun example-charp (p)
+  (let ((cf (make-srcf (finite-fields:with-modulus (p)
+                         (make-polynomial 1 0 0 0 0 1 1)))))
+    (srcf-quasi-period cf 500)))
