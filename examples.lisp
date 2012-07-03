a94f61d679f0c950ace33bd5cd36f52bf8f5b9c6
Olaf Merkert <olaf@m-merkert.de>
2012-07-03 10:19:50 +0200

    apply power-series api changes

--- examples.lisp
+++ examples.lisp
@@ -1,33 +1,33 @@
 (in-package :cf-ps)
 
 (defun example-9 ()
-  (let* ((d (power-series::make-power-series/polynomial
+  (let* ((d (make-power-series nil
              1 -8 -42 424 -119))
          (cq (complete-quotients d)))
     (cq-period cq)))
 
 (defun example-9/q ()
-  (let* ((d (power-series::make-power-series/polynomial
+  (let* ((d (make-power-series nil
              1 -8 -42 424 -119))
          (cq (complete-quotients d)))
     (cq-quasi-period cq)))
 
 (defun example-char0 (bound)
-  (let* ((d (power-series::make-power-series/polynomial
+  (let* ((d (make-power-series nil
              1 0 0 0 0 1 1))
          (cq (complete-quotients d)))
     (cq-period cq bound)))
 
 (defun example-charp (p)
    (let* ((d (finite-fields:with-modulus (p)
-               (power-series::make-power-series/polynomial
+               (make-power-series nil
               1 0 0 0 0 1 1)))
          (cq (complete-quotients d)))
     (cq-period cq 500)))
 
 (defun example-charp/q (p)
    (let* ((d (finite-fields:with-modulus (p)
-               (power-series::make-power-series/polynomial
+               (make-power-series nil
               1 0 0 0 0 1 1)))
          (cq (complete-quotients d)))
     (cq-quasi-period cq 500)))
