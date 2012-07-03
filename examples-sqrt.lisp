ad40a5f9fa6a63e784cb3289efd19c4543f83f1c
Olaf Merkert <olaf@m-merkert.de>
2012-07-03 20:03:47 +0200

    check the pell equation and compare with older results

--- examples-sqrt.lisp
+++ examples-sqrt.lisp
@@ -3,14 +3,36 @@
 (defun example-9 ()
   (let ((cf (make-srcf (make-polynomial
                         1 -8 -42 424 -119))))
-    (srcf-quasi-period cf)))
+    (list
+     :period (srcf-quasi-period cf)
+     :cf cf
+     :d (d cf)
+     :pn (pn (an cf))
+     :qn (qn (an cf)))))
 
 (defun example-char0 (bound)
   (let ((cf (make-srcf 
              (make-polynomial 1 0 0 0 0 1 1))))
-    (srcf-quasi-period cf bound)))
+    (list
+     :period (srcf-quasi-period cf bound)
+     :cf cf
+     :d (d cf)
+     :pn (pn (an cf))
+     :qn (qn (an cf)))))
 
 (defun example-charp (p)
   (let ((cf (make-srcf (finite-fields:with-modulus (p)
                          (make-polynomial 1 0 0 0 0 1 1)))))
-    (srcf-quasi-period cf 500)))
+    (list
+     :period (srcf-quasi-period cf 500)
+     :cf cf
+     :d (d cf)
+     :pn (pn (an cf))
+     :qn (qn (an cf)))))
+
+(defun test-pell/index (result &optional index)
+  (destructuring-bind (&key pn qn period d cf) result
+    (unless index (setf index (- period 1)))
+   (test-pell (lazy-aref pn index)
+              (lazy-aref qn index)
+              d)))
