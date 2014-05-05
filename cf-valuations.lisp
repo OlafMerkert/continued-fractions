b1986a702c7de38dc255b95368c1d42e68691333
Olaf Merkert <olaf@m-merkert.de>
2014-05-05 09:54:41 +0200

    transition to `tn' instead of `rn' also in valuations part

--- cf-valuations.lisp
+++ cf-valuations.lisp
@@ -45,7 +45,7 @@
                  cf-ps::approx-numerators
                  cf-ps::approx-denominators
                  cf-ps::radicand
-                 (rrn cf-ps::rn)
+                 (ttn cf-ps::tn)
                  (ssn cf-ps::sn)) scf
       (with-cf2 sqrt-continued-fraction
         (setf cf-ps::starting            (vc:valuate-exp valuation alpha0)
@@ -54,39 +54,6 @@
               cf-ps::approx-numerators   (map-sequence v pn)
               cf-ps::approx-denominators (map-sequence v qn)
               cf-ps::radicand            (vc:valuate-exp valuation d)
-              rrn                 (map-sequence v rn)
+              ttn                 (map-sequence v tn)
               ssn                 (map-sequence v sn))))
     scf))
-
-(defclass symbolic-quadratic-continued-fraction (symbolic-continued-fraction
-                                                 quadratic-continued-fraction)
-  ())
-
-(defmethod vc:valuate-exp (valuation (quadratic-continued-fraction quadratic-continued-fraction))
-  (let ((scf (make-instance 'symbolic-quadratic-continued-fraction))
-        (v (vc:val valuation)))
-    (with-slots (cf-ps::starting
-                 cf-ps::complete-quotients
-                 cf-ps::partial-quotients
-                 cf-ps::approx-numerators
-                 cf-ps::approx-denominators
-                 cf-ps::radicand
-                 (aa cf-ps::a)
-                 (bb cf-ps::b)
-                 (cc cf-ps::c)
-                 (rrn cf-ps::rn)
-                 (ssn cf-ps::sn)
-                 (ttn cf-ps::tn)) scf
-      (with-cf2* quadratic-continued-fraction
-        (setf cf-ps::starting            (vc:valuate-exp valuation alpha0)
-              cf-ps::complete-quotients  (map-sequence v alphan )
-              cf-ps::partial-quotients   (map-sequence v an)
-              cf-ps::approx-numerators   (map-sequence v pn)
-              cf-ps::approx-denominators (map-sequence v qn)
-              cf-ps::radicand            (vc:valuate-exp valuation d)
-              aa                         (vc:valuate-exp valuation cf-ps::a)
-              bb                         (vc:valuate-exp valuation cf-ps::b)
-              cc                         (vc:valuate-exp valuation cf-ps::c)
-              rrn                        (map-sequence v rn)
-              ssn                        (map-sequence v sn)
-              ttn                        (map-sequence v tn))))))
